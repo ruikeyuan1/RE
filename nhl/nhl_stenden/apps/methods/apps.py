ba50d3bdc9a7b580ec0170cb43e35be135410018
ruikeyuan1 <94407303+ruikeyuan1@users.noreply.github.com>
2022-07-27 12:16:12 +0200

    Delete nhl directory

--- nhl/nhl_stenden/apps/methods/apps.py
+++
@@ -1,7 +0,0 @@
-from tabnanny import verbose
-from django.apps import AppConfig
-
-class MethodsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'methods'
-    verbose_name = "Methods"
--- nhl/nhl_stenden/apps/methods/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('registration/', views.registration, name="registration"),
-    path('getroom/', views.getroom, name="getroom"),
-]
